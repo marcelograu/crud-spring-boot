865edeff11a6cf992abc49e48455e1d632f31779
MarceloGrauBetim <marcelograubetim@gmail.com>
2024-06-15 16:55:28 -0300

    Criando as anotações

---
+++ src/main/java/com/sgn/exemplocrud/entities/SystemUser.java
@@ -0,0 +1,83 @@
+package com.sgn.exemplocrud.entities;
+
+import java.time.LocalDate;
+
+import com.sgn.exemplocrud.entities.enuns.UserRole;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "system_user")
+public class SystemUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "role")
+    private UserRole role;
+
+    @Column(name = "pass_word")
+    private String passWord;
+
+    @Column(name = "creation_date")
+    private LocalDate creationDate;
+
+    public SystemUser() {
+
+    }
+
+    public SystemUser(String name, UserRole role, String passWord, LocalDate creationDate) {
+        this.name = name;
+        this.role = role;
+        this.passWord = passWord;
+        this.creationDate = creationDate;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+}
